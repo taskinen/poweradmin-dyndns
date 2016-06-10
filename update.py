9c737d9b74071113b34b2d4ab12b09cb86a05858
Timo Taskinen <taskinen@koti.xk.fi>
2016-06-10 11:45:14 +0300

    Add verbose option. More error handling.

--- update.py
+++ update.py
@@ -15,14 +15,29 @@ if config['default']['poweradmin_url'][:8] != "https://":
   sys.exit(1)
 
 # Find external IP
+if config['default'].getboolean('verbose'):
+  print( "Querying public IP from: " + config['default']['ip_lookup_url'] )
 r = requests.get( config['default']['ip_lookup_url'] )
 if r.status_code is not 200:
-  print( "Error finding external IP from output: " + r.text )
+  print( "ERROR: Querying external IP failed with response " + r.status_code )
   sys.exit(1)
-ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', r.text )[0]
-print( "Current external IP: " + ip )
+ip = re.search( r'[0-9]+(?:\.[0-9]+){3}', r.text )
+if ip:
+  if config['default'].getboolean('verbose'):
+    print( "Current external IP: " + ip.group(0) )
+else:
+  print( "ERROR: Not able to parse external IP from output: " + ip.string )
+  sys.exit(1)
 
 # Update IP
-url = config['default']['poweradmin_url'] + '/dynamic_update.php' + '?hostname=' + config['default']['domain']+ '&myip=' + ip
+url = config['default']['poweradmin_url'] + '/dynamic_update.php' + '?hostname=' + config['default']['domain']+ '&myip=' + ip.group(0)
+if config['default'].getboolean('verbose'):
+  print( "Updating hostname " + config['default']['domain'] + " point to " + ip.group(0) + " using " + url )
 r = requests.get( url, auth=( config['default']['login'], config['default']['password'] ) )
-print( "Poweradmin says: " + r.text )
+if r.text.strip() != "good":
+  print( "ERROR: Not able to update hostname. Poweradmin responded: " + r.text.strip() )
+  sys.exit(1)
+elif config['default'].getboolean('verbose'):
+  print( "Poweradmin responds: " + r.text.strip() )
+
+sys.exit(0)
